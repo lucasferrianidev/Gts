c74cd97eb15ccb36821519bbaf4454cf376b90bc
Lucas Ferriani <lucasferrianidev@gmail.com>
2023-04-27 13:08:45 -0300

    Adicionado validação no modelo Profissional

---
+++ Gst/Controllers/ProfissionalController.cs
@@ -0,0 +1,20 @@
+﻿using Gst.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Gst.Controllers;
+
+[ApiController]
+[Route("[controller]")]
+public class ProfissionalController : ControllerBase
+{
+    List<Profissional> profissionais = new List<Profissional>();
+
+    [HttpPost]
+    public void AdicionarProfissional([FromBody] Profissional profissional)
+    {
+        profissionais.Add(profissional);
+        Console.WriteLine(profissional.Nome);
+        Console.WriteLine(profissional.Comissao);
+        Console.WriteLine(profissional.DataNascimento);
+    }
+}
---
+++ Gst/Models/Profissional.cs
@@ -0,0 +1,18 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Gst.Models;
+
+public class Profissional
+{
+    [Required(ErrorMessage = "O nome é obrigatório")]
+    public string Nome { get; set; }
+
+    [Required(ErrorMessage = "A data de nascimento é obrigatório")]
+    public DateTime DataNascimento { get; set; }
+
+    [Required(ErrorMessage = "O CPF é obrigatório")]
+    public string Cpf { get; set; }
+
+    [Range(0, 100, ErrorMessage = "A comissão deve ser entre 0 e 100 porcento")]
+    public decimal Comissao { get; set; }
+}
